089495649ff0aa291b3bdf27130daf6ad0ea4249
sosky-yty <1027797811@qq.com>
2018-09-07 00:34:51 +0800

    基本样式导入完成，静态图片资源导入

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view class="main" />
   </div>
 </template>
 
@@ -11,13 +10,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" rel="stylesheet/scss">
+  @import "assets/style/index.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%
+}
+
+.main{
+  background: #ededed;
 }
 </style>
