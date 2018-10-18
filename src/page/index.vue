9c83d8d151b0b051e1a5b125296b8dc2ae5ec3c4
sosky-yty <1027797811@qq.com>
2018-10-18 11:10:54 +0800

    修改index.vue

--- src/page/index.vue
+++ src/page/index.vue
@@ -4,12 +4,12 @@
     <router-view class="main"></router-view>
     <y-footer></y-footer>
     <!--抛物图片-->
-    <!-- <transition @after-enter='afterEnter' @before-enter="beforeEnter">
+    <transition @after-enter='afterEnter' @before-enter="beforeEnter">
       <div class="move_img" v-if="showMoveImg"
            :style="{left:(cartPositionL-10) + 'px',top:(cartPositionT-10) + 'px'}">
         <div><img :src="moveImgUrl"></div>
       </div>
-    </transition> -->
+    </transition>
   </div>
 </template>
 <script>
