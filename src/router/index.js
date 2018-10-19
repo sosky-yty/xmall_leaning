90bc99dba2c04877925b63bac27d0d90f159c372
sosky-yty <1027797811@qq.com>
2018-10-19 11:37:17 +0800

    注册页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 const Index = () => import('../page/index.vue')
 const Login = () => import('../page/Login/login.vue')
+const Register = () => import('../page/Login/register.vue')
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -10,6 +11,7 @@ export default new Router({
       name: 'index',
       component: Index
     },
-    {path: '/login', name: 'login', component: Login}
+    {path: '/login', name: 'login', component: Login},
+    {path: '/register', name: 'register', component: Register}
   ]
 })
